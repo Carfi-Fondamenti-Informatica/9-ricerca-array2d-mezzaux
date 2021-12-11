b91d2616cef3b9c53ab24e143c92b61e26aa504f
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-11 21:07:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int cerca(char a[10][20], char nome[]);
+
 int main() {
     char lista[10][20] = {0};
     char nome[20] = {0};
     for (size_t i = 0; i < 10; i++) cin >> lista[i];
+    cout<< endl;
     nome[20];
     for (int j=0; j<20; j++) nome[j];
     cin >> nome;
@@ -15,7 +16,4 @@ int main() {
     } else {
         cout << "non presente" << endl;
     }
-}
-
-    return 0;
 }
