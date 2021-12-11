e1820bedecaf90790af0db9e424055c5312101ff
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-12-11 21:00:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,6 @@ int cerca(char a1[10][20], char a2[20]) {
 
     for (int i = 0; i < 10; i++) {
         bool flag = true;
-
         for (int j = 0; j < 20; j++) {
             if (a1[i][j] != a2[j]) flag = false;
         }
